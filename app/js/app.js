e2c42484db89b3b4c1b57bd6f6d4f43223d689ff
Artem Fedchenko <artemwebworking@gmail.com>
2024-06-27 12:44:25 +0300

    added: subscribe section, footer

--- app/js/app.js
+++ app/js/app.js
@@ -31,7 +31,58 @@ document.addEventListener('DOMContentLoaded', () => {
 		  });
 	}
 
+	function form() {
+		let phoneWrapper = document.querySelector('.phone_wrapper');
+		let formAnswer = document.querySelector('.subscribe-main-form-answer');
+		let phoneInput = document.getElementById('phone_input');
+	
+		$('#form').submit(function(event) {
+			event.preventDefault();
+			let phoneInputValue = phoneInput.value.trim();
+	
+			// Валидация телефонного номера
+			if (validatePhone(phoneInputValue)) {
+				// Успешная валидация
+				var form_data = $(this).serialize();
+				$.ajax({
+					type: 'POST',
+					url: './send.php',
+					data: form_data,
+					success: function(data) {
+						formAnswer.innerHTML = 'Sending successful';
+						formAnswer.classList.add('success');
+						$('#form')[0].reset();
+						setTimeout(() => {
+							formAnswer.classList.remove('success');
+						}, 1000);
+					},
+					error: function(data) {
+						formAnswer.classList.add('active');
+						formAnswer.innerHTML = 'Error';
+					}
+				});
+			} else {
+				// Ошибка валидации
+				console.log('Validation error');
+				phoneWrapper.classList.add('error');
+				formAnswer.classList.add('active');
+				formAnswer.innerHTML = 'Please enter a valid phone number';
+			}
+		});
+	
+		phoneInput.addEventListener('input', () => {
+			formAnswer.classList.remove('active');
+			phoneWrapper.classList.remove('error');
+		});
+	
+		function validatePhone(phone) {
+			// Проверка на пустоту и формат номера
+			const re = /^\d+$/;
+			return re.test(phone);
+		}
+	}
 
+	form()
 	swiperSlider()
 
 })
